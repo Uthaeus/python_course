d4ace62f9025dd2b60180b1af77ee36c9847cfd9
Uthaeus <Uth@github.com>
2018-02-18 00:45:56 -0700

    string case functions

---
+++ notes/string_case_functions.py
@@ -0,0 +1,24 @@
+
+sentence = 'The quick brown fox jumped'.upper()
+
+print(sentence)
+
+sentence.upper() # <= does not work
+
+print(sentence.upper()) # <= this will work
+
+sentence_two = sentence.upper()
+
+print(sentence_two)
+
+sentence = 'the quick brown fox jumped'.capitalize()
+
+print(sentence)
+
+sentence = 'the quick brown fox jumped'.title()
+
+print(sentence)
+
+sentence = 'THE QUICK BROWN FOX JUMPED'.lower()
+
+print(sentence)
