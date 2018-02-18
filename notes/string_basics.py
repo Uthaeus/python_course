3721d012a563c27728aef678c55fb80c76fdef1a
Uthaeus <Uth@github.com>
2018-02-18 00:32:17 -0700

    string basics

---
+++ notes/string_basics.py
@@ -0,0 +1,16 @@
+
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+print(sentence)
+#will print out the sentence
+
+#can use single or double quotes for interpolation
+
+sentence_two = "That is my dog's bowl"
+# backslash \ is used as an escape character
+
+print(sentence_two)
+
+sentence_four = "Tiffany said, \"That is my dog's bowl\""
+print(sentence_four)
+
