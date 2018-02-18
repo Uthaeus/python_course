a3b2e62691426cc8bfc01f882fc987768d216468
Uthaeus <Uth@github.com>
2018-02-18 00:03:42 -0700

    adding to data types

--- notes/python_data_types.py
+++ notes/python_data_types.py
@@ -9,3 +9,18 @@
 # Tuples
 # Sets
 # Dictionaries
+
+meal_completed = True
+sub_total = 100
+tip = sub_total * 0.2
+
+#print(sub_total)
+    # => 100
+
+total = sub_total + tip
+
+#print(total)
+
+receipt = "Your total is " + str(total)
+
+print(receipt)
