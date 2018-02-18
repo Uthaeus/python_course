7ce8395acc3451cbb2e14d3d24443624ec9e1023
Uthaeus <Uth@github.com>
2018-02-18 00:11:17 -0700

    variable values

---
+++ notes/variable_values.py
@@ -0,0 +1,21 @@
+meal_completed = True
+total = 100
+tip = total * 1/5
+total = total + tip
+receipt = "Your total is " + str(total)
+
+print(receipt)
+
+
+first = "Springer"
+second = "Bregman"
+third = "Altuve"
+
+print(second)
+
+second = "Correa"
+
+print(second)
+# swapping out values
+
+second = 0.280
