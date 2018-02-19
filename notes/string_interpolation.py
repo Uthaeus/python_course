4d45c58dd2e44d8eecf3e683c611f7052d841844
Uthaeus <Uth@github.com>
2018-02-18 20:11:15 -0700

    string interpolation

---
+++ notes/string_interpolation.py
@@ -0,0 +1,24 @@
+
+name = 'Kristine'
+
+greeting = f'Hi {name}'
+print(greeting)
+
+# How to show brackets in a string
+
+sentence = f'This is my {{bracket}} blog post'
+
+name = 'Kristine'
+product = 'Python elearning course'
+
+email_content = f"""
+Hi {name}
+
+Thank you for purchasing {product}
+
+Regards, 
+
+Sales Team
+"""
+
+print(email_content)
