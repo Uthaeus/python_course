191fdc4726552bf1aec071ef4f3964d491c07178
Uthaeus <Uth@github.com>
2018-02-26 20:27:18 -0700

    How to sort lists

---
+++ notes/sort_lists.py
@@ -0,0 +1,19 @@
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+print(tags)
+
+tags.sort() # Alphabetical order a - z
+
+print(tags)
+
+tags.sort(reverse=True) # z - a
+
+print(tags)
+
+totals = [234, 1, 543, 2345]
+totals.sort() # Sorts smallest to largest
+totals.sort(reverse=True) # Largest to smallest
+
+print(totals)
+
