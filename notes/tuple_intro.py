80785a1b843801e6b52271d22270463429d55745
Uthaeus <Uth@github.com>
2018-02-28 10:24:38 -0700

    Intro to tuples

---
+++ notes/tuple_intro.py
@@ -0,0 +1,15 @@
+# List: []
+# Dictionary: {}
+# Tuple: ()
+
+# Tuple: immutable
+# List: mutable
+
+post = ('Python Basics', 'Intro guide to python', 'Some python content')
+
+title, sub_heading, content = post
+
+# title = post[0]
+# sub_heading = post[1]
+# content = post[2]
+
