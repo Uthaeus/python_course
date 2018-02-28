90f2921c7a1d1e3e33d099e5a87cfb8070df1db9
Uthaeus <Uth@github.com>
2018-02-28 12:42:05 -0700

    How to slice tuples

---
+++ notes/tuple_slice.py
@@ -0,0 +1,7 @@
+
+post = ('Python Basics', 'Intro guide to Python', 'Some python content', 'published')
+
+print(post[:2]) # => ('Python Basics', 'Intro guide to Python')
+
+print(post[1::2]) # => ('Intro guide to Python', 'published')
+  # => The last element here is the interval count
