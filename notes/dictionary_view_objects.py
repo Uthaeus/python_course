e735283b676554447dd84ca85c51db08a1ea192e
Uthaeus <romanlavery@gmail.com>
2018-02-28 22:05:38 -0700

    conditionals, view objects, loops, methods

---
+++ notes/dictionary_view_objects.py
@@ -0,0 +1,19 @@
+
+players = {
+  "ss" : "Correa",
+  "2b" : "Altuve",
+  "3b" : "Bregman",
+  "DH" : "Gattis",
+  "OF" : "Bregman"
+}
+
+print(players.keys()) # => dict_keys (dictionary view objects)
+print(players.values())
+print(players.items())
+
+teams = {
+  "astros" : ["Altuve", "Correa", "Bregman"],
+  "angels" : ["Trout", "Pujols"],
+  "yankees" : ["Judge", "Stanton"],
+  "red sox" : ["Price", "Betts"]
+}
---
+++ notes/loops1.py
@@ -0,0 +1,4 @@
+# There are two different types of loops in Python
+# For in loops
+# While loops
+
